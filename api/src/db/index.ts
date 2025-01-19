1c44286cf7b91413b404f6471d071de7b617cd0c
oottoohh <ottoh.hidayatullah@ai.astra.co.id>
2025-01-19 21:58:33 +0700

    Drizzle setup

---
+++ api/src/db/index.ts
@@ -0,0 +1,11 @@
+import { drizzle } from "drizzle-orm/node-postgres";
+import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+
+export const usersTable = pgTable("users", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  name: varchar({ length: 255 }).notNull(),
+  age: integer().notNull(),
+  email: varchar({ length: 255 }).notNull().unique(),
+});
+
+export const db = drizzle(process.env.DATABASE_URL!);
---
+++ api/src/db/schema.ts
@@ -0,0 +1,5 @@
+import * as productSchema from "./productsSchema";
+
+export default {
+  ...productSchema,
+};
