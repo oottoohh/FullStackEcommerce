d0a5bfbcd603145276acaf6c7b8483997d2803e8
oottoohh <ottoh.hidayatullah@ai.astra.co.id>
2025-01-29 21:36:14 +0700

    feat : setup gluestack

--- ecommerce-mobile/app/_layout.tsx
+++ ecommerce-mobile/app/_layout.tsx
@@ -1,6 +1,12 @@
 import React from "react";
 import { Slot, Stack, Tabs } from "expo-router";
+import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
+import "@/global.css";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <GluestackUIProvider>
+      <Stack />
+    </GluestackUIProvider>
+  );
 }
--- ecommerce-mobile/app/index.tsx
+++ ecommerce-mobile/app/index.tsx
@@ -1,11 +1,14 @@
 import { View, Text, FlatList } from "react-native";
 import products from "../assets/products.json";
-import ProductListItem from "../components/ProductListItem";
+import ProductListItem from "@/components/ProductListItem";
 
 export default function HomeScreen() {
   return (
     <FlatList
       data={products}
+      numColumns={2}
+      contentContainerClassName="gap-2"
+      columnWrapperClassName="gap-2"
       renderItem={({ item }) => <ProductListItem product={item} />}
       keyExtractor={(item) => item.id.toString()}
     />
